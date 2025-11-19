5c061f881481cdbe88d26db00bc90c7ed9c6f61d
Nayeli Morales <moralesmedinanayeli@gmail.com>
2025-11-19 14:11:25 -0600

    feat: Fase 1 completa (Funciones suma, resta, multiplicacion, division completas)

---
+++ script.js
@@ -0,0 +1,27 @@
+function sumar(x, y){
+    return x + y;
+}
+
+function resta(x, y){
+    return x - y;
+}
+
+function multiplicación(x, y){
+    return x * y;
+}
+
+function division(x, y){
+    return x / y;
+}
+
+let resultado = sumar(5,5);
+console.log("resultado de suma" + " " + resultado);
+
+resultado = resta(5,5);
+console.log("resultado de resta" + " " + resultado);
+
+resultado = multiplicación(5,5);
+console.log("resultado de multiplicacion" + " " + resultado);
+
+resultado = division(5,5);
+console.log("resultado de multiplicación" + " " + resultado);
