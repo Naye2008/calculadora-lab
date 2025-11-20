75566e4114b56265cd69185d21a6bf05b0fdedd4
Nayeli Morales <moralesmedinanayeli@gmail.com>
2025-11-19 23:07:07 -0600

    feat: Pruebas finales (Console logs demostrando que la calculadora funciona).

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let numero1= parseFloat("5");
-let numero2= parseFloat("5");
+let numero1= parseFloat(prompt("Ingrese el el primer  numero:"));
+let numero2= parseFloat(prompt("Ingrese el el segundo  numero:"));
 
 function sumar(numero1, numero2){
     return numero1 + numero2;
@@ -18,3 +18,12 @@ function division(numero1, numero2){
 }
 let resultado = sumar(numero1, numero2);
 console.log("resultado de suma" + " " + resultado);
+
+resultado = resta(numero1, numero2);
+console.log("resultado de resta" + " " + resultado);
+
+resultado = multiplicacion(numero1, numero2);
+console.log("resultado de multiplicacion" + " " + resultado);
+
+resultado = division(numero1, numero2);
+console.log("resultado de division" + " " + resultado);
